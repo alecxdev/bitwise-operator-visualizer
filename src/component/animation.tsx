1dda91a601bca58ff5fa10835cbbcf1a3c7c7fd8
Alejandro <alexchuc84@gmail.com>
2024-11-11 23:21:02 -0600

    Add animation component

---
+++ src/component/animation.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+interface ChildrenProps {
+    animation: string;
+    startAnimation: (props?: { delay?: number }) => void;
+}
+
+interface Props {
+    children: (props: ChildrenProps) => React.ReactNode;
+}
+export const Animation = ({ children }: Props) => {
+    const [animate, setAnimate] = useState(false);
+    const timeoutRef = useRef<number>();
+
+    useEffect(() => () => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }, []);
+
+    const startAnimation = useCallback((props: Parameters<ChildrenProps['startAnimation']>[0]) => {
+        setAnimate(true);
+        const delay = props?.delay ?? 0;
+        
+        timeoutRef.current = setTimeout(() => {
+            setAnimate(false);
+        }, 1500 + delay);
+    }, []);
+    
+    return (
+        children({
+            // if animation ends, return to opacity 0,
+            // because animation only takes the time that you typed on animation class
+            animation: `show ${animate ? 'show-start' : 'show-end'}`,
+            startAnimation
+        })
+    );
+}
