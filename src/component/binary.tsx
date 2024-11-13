506ae5c30f497e5d77f278ef78c49aef34d8ec90
Alejandro <alexchuc84@gmail.com>
2024-11-12 23:34:45 -0600

    Add animation to binary nums

--- src/component/binary.tsx
+++ src/component/binary.tsx
@@ -1,8 +1,16 @@
 type Props = React.HTMLAttributes<HTMLSpanElement> & {
     children: number;
     bitSize?: number;
+    extraBitProps?: React.HTMLAttributes<HTMLSpanElement>;
 }
 
-export const Binary = ({ children, bitSize: maxSize = 0, ...props }: Props) => (
-    <span {...props}>{children.toString(2).padStart(maxSize, '0')}</span>
-);
+export const Binary = ({ children, extraBitProps: extraProps, bitSize: maxSize = 0, ...props }: Props) => {
+    const size = maxSize - children.toString(2).length;
+
+    return (
+        <span {...props}>
+            {size > 0 ? <span {...extraProps}>{'0'.repeat(size)}</span> : null}
+            {children.toString(2)}
+        </span>
+    );
+};
--- src/component/operator.tsx
+++ src/component/operator.tsx
@@ -2,6 +2,7 @@ import { Fragment, useMemo, useState } from 'react';
 import { Binary } from './binary';
 import { Bitwise, BITWISE_OPERATORS } from '../models';
 import { andFn, orFn, xorFn } from '../services/bitwise';
+import { Animation } from './animation';
 
 export const Operator = ({ nums, operator }: { nums: number[]; operator: Bitwise }) => {    
     const [maxSize, setMaxSize] = useState(0);
@@ -36,18 +37,30 @@ export const Operator = ({ nums, operator }: { nums: number[]; operator: Bitwise
         <div className='grid grid-cols-[1fr_1fr] gap-y-6'>
             <span className='text-center'>Binary</span>
             <span>Decimal</span>
-            {nums.map((num, i) => (
-                <Fragment key={i}>
-                    <Binary
-                    className='odd:bg-gray-100 rounded-l-lg text-right'
-                    bitSize={maxSize}
-                    >{num}</Binary>
-                    {/* <button>^</button>
-                    <button>|</button>
-                    <button>&</button> */}
-                    <span className='even:bg-gray-100 rounded-r-lg'>=&emsp;{num}</span>
-                </Fragment>
-            ))}
+            <Animation>
+                {(props) => (
+                    <>
+                    {nums.map((num, i) => (
+                        <Fragment key={i}>
+                            <Binary
+                            className='odd:bg-gray-100 rounded-l-lg text-right'
+                            extraBitProps={{
+                                className: props.animation,
+                                style: { animationDelay: `${i}000ms`}
+                            }}
+                            bitSize={maxSize}
+                            >{num}</Binary>
+                            {/* <button>^</button>
+                            <button>|</button>
+                            <button>&</button> */}
+                            <span className='even:bg-gray-100 rounded-r-lg'>=&emsp;{num}</span>
+                        </Fragment>
+                    ))}
+                    <span />
+                    <button onClick={() => props.startAnimation({ delay: (nums.length - 1) * 1000 })}>start</button>
+                    </>
+                )}
+            </Animation>
         </div>
             <hr className='my-4 border-b w-full' />
             <div>=&emsp;<Binary className="font-bold">{total}</Binary> -&gt; {total}</div>
